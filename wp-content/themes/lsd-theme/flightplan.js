4d22b040a226a599763a739187a77fb1ba32e28c
jake kortbeek <jakekortbeek@jakes-iMac.local>
2018-03-19 23:56:43 +0000

    css folder

---
+++ wp-content/themes/lsd-theme/flightplan.js
@@ -0,0 +1,46 @@
+var plan = require('flightplan');
+var webRoot = {
+  staging: 'subdomains/dev/lsd/wp-content/themes/lsd-theme',
+  production: 'path/to/staging/wordpress/theme'
+};
+
+plan.target('staging', [
+  {
+    host: 'shell.gridhost.co.uk',
+    username: 'kortbeek',
+    password: 'm91ig25ne6btksq1',
+    agent: process.env.SSH_AUTH_SOCK
+  }
+], {
+  webRoot: webRoot.staging
+});
+
+plan.target('production', [
+  {
+    host: 'host',
+    username: 'username',
+    password: 'password',
+    privateKey: 'path/to/file/key.pem',
+    agent: process.env.SSH_AUTH_SOCK
+  }
+], {
+  webRoot: webRoot.production
+});
+
+plan.local(function (local) {
+  local.log('Searching for file to upload');
+  var filesToCopy = local.exec('git ls-files', {silent: true});
+  local.log('Search Complete.........');
+  var webRoot = plan.runtime.options.webRoot;
+  local.log('Uploading files to :: ' + webRoot);
+  local.transfer(filesToCopy, webRoot);
+  local.log('Uploading Complete.......');
+});
+
+plan.remote(function (remote) {
+  var webRoot = plan.runtime.options.webRoot;
+  remote.log('changing permission of all files to 644');
+  remote.exec('find ' + webRoot + ' -type f -exec chmod 644 {} +');
+  remote.log('changing permission of all folders to 755');
+  remote.exec('find ' + webRoot + ' -type d -exec chmod 755 {} +');
+});
