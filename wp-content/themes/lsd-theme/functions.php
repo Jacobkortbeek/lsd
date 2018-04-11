312357acff7d56c4d5c4cb36136752236257b4b3
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-11 20:09:11 +0100

    Base template

--- wp-content/themes/lsd-theme/functions.php
+++ wp-content/themes/lsd-theme/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page();
+
+}
+
 function wpt_theme_styles() {
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'secondary_css', get_template_directory_uri() . '/css/style.css' );
