bc72b2ed3772363ce1af55c829e3171f1ead5cfc
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-11 20:19:19 +0100

    header

--- wp-content/themes/lsd-theme/functions.php
+++ wp-content/themes/lsd-theme/functions.php
@@ -1,5 +1,17 @@
 <?php
 
+function wpt_create_widget( $name, $id, $description ) {
+  register_sidebar(array(
+    'name' => __( $name ),
+    'id' => $id,
+    'description' => __( $description ),
+    'before_widget' => '<div class="header">',
+    'after_widget' => '</div>',
+    'before_title' => '<h2 class="heading">',
+    'after_title' => '</h2>'
+  ));
+}
+
 wpt_create_widget( 'Page Header', 'page', 'Header' );
 
 function wpt_theme_styles() {
