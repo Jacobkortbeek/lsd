5ccb3279e70e3e07674f0c60378d63d70ba0a5b5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 00:49:52 +0100

    front page start php

--- wp-content/themes/lsd-theme/page-front.php
+++ wp-content/themes/lsd-theme/page-front.php
@@ -3,3 +3,18 @@
     Template Name: front page
   */
 ?>
+
+<div class="row">
+  <div class="col-12">
+    <?php $logo = get_field( 'logo' ); ?>
+    <?php if ( $logo ) { ?>
+	     <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
+    <?php } ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12">
+    <?php the_field( 'text_box' ); ?>
+  </div>
+</div>
