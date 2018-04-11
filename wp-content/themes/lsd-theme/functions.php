727103ea006a1def2179cd36f61222b984eb2bbb
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-11 18:26:37 +0100

    new style sheet

--- wp-content/themes/lsd-theme/functions.php
+++ wp-content/themes/lsd-theme/functions.php
@@ -2,6 +2,7 @@
 
 function wpt_theme_styles() {
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'secondary_css', get_template_directory_uri() . '/css/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
