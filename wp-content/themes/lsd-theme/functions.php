0df308489d7818b3a1a6d3931a7cc91d49a9a22b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 00:33:00 +0100

    bootstrap

--- wp-content/themes/lsd-theme/functions.php
+++ wp-content/themes/lsd-theme/functions.php
@@ -17,7 +17,13 @@ wpt_create_widget( 'Page Header', 'page', 'Header' );
 function wpt_theme_styles() {
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'secondary_css', get_template_directory_uri() . '/css/style.css' );
+  wp_enqueue_style( 'bootstrap_responsive', get_template_directory_uri() . '/css/bootstrap-responsive.min.css' );
+  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+function wpt_theme_js() {
+  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js');
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
 
 ?>
