8ea4ccb98f4b3e4eb466f5012a1baaaef8997bce
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-11 19:39:10 +0100

    Base template

--- wp-content/themes/lsd-theme/page-common.php
+++ wp-content/themes/lsd-theme/page-common.php
@@ -3,3 +3,10 @@
     Template Name: Base Template
   */
 ?>
+
+<?php the_field( 'heading' ); ?>
+<?php the_field( 'text' ); ?>
+<?php $image = get_field( 'image' ); ?>
+<?php if ( $image ) { ?>
+	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+<?php } ?>
