1a56de08accc2270eac0004173f126c4318047c7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 20:58:45 +0100

    blob

--- wp-content/themes/lsd-theme/header.php
+++ wp-content/themes/lsd-theme/header.php
@@ -7,37 +7,6 @@
     <?php wp_head(); ?>
   </head>
   <header>
-    <div class="blob-svg">
-      <svg
- xmlns="http://www.w3.org/2000/svg"
- xmlns:xlink="http://www.w3.org/1999/xlink"
- width="35.489cm" height="28.469cm">
-<defs>
-<filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="1006px" height="807px"  >
-    <feOffset in="SourceAlpha" dx="-12.876" dy="21.429" />
-    <feGaussianBlur result="blurOut" stdDeviation="7.746" />
-    <feFlood flood-color="rgb(0, 0, 0)" result="floodOut" />
-    <feComposite operator="atop" in="floodOut" in2="blurOut" />
-    <feComponentTransfer><feFuncA type="linear" slope="0.25"/></feComponentTransfer>
-    <feMerge>
-    <feMergeNode/>
-    <feMergeNode in="SourceGraphic"/>
-  </feMerge>
-</filter>
-<linearGradient id="PSgrad_0" x1="0%" x2="0%" y1="0%" y2="100%">
-  <stop offset="0%" stop-color="rgb(153,255,125)" stop-opacity="1" />
-  <stop offset="30%" stop-color="rgb(125,255,214)" stop-opacity="1" />
-  <stop offset="70%" stop-color="rgb(125,238,255)" stop-opacity="1" />
-  <stop offset="100%" stop-color="rgb(125,156,255)" stop-opacity="1" />
-</linearGradient>
-
-</defs>
-<g filter="url(#Filter_0)">
-<path fill-rule="evenodd"  fill="url(#PSgrad_0)"
- d="M74.000,52.000 C74.000,52.000 105.487,256.459 288.000,302.000 C470.513,347.541 580.619,421.004 597.000,501.000 C613.381,580.996 669.484,744.273 867.000,721.000 C1064.515,697.727 875.000,40.000 875.000,40.000 L74.000,52.000 Z"/>
-</g>
-</svg>
-    </div>
     <div class="row">
       <div class="span12 header-container">
         <h2>Jacob Kortbeek</h2>
