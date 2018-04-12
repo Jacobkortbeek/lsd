992ff2cf03857072cedae4254a868ad1d90d1b81
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 01:00:34 +0100

    front page start php

--- wp-content/themes/lsd-theme/page-front.php
+++ wp-content/themes/lsd-theme/page-front.php
@@ -21,4 +21,52 @@
   </div>
 </div>
 
+<div class="row">
+  <div class="col-12">
+    <?php $blog_post_loop = get_field( 'blog_post_loop' ); ?>
+    <?php var_dump( $blog_post_loop ); ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+    <?php $image_one = get_field( 'image_one' ); ?>
+<?php if ( $image_one ) { ?>
+	<img src="<?php echo $image_one['url']; ?>" alt="<?php echo $image_one['alt']; ?>" />
+<?php } ?>
+  </div>
+  <div class="col-md-4">
+    <?php $image_two = get_field( 'image_two' ); ?>
+<?php if ( $image_two ) { ?>
+	<img src="<?php echo $image_two['url']; ?>" alt="<?php echo $image_two['alt']; ?>" />
+<?php } ?>
+  </div>
+  <div class="col-md-4">
+    <?php $image_three = get_field( 'image_three' ); ?>
+<?php if ( $image_three ) { ?>
+	<img src="<?php echo $image_three['url']; ?>" alt="<?php echo $image_three['alt']; ?>" />
+<?php } ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-12">
+    <?php the_field( 'button' ); ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-6">
+    <?php $portfolio_1 = get_field( 'portfolio_1' ); ?>
+<?php if ( $portfolio_1 ) { ?>
+	<img src="<?php echo $portfolio_1['url']; ?>" alt="<?php echo $portfolio_1['alt']; ?>" />
+<?php } ?>
+  </div>
+  <div class="col-md-6">
+    <?php $portfolio_2 = get_field( 'portfolio_2' ); ?>
+<?php if ( $portfolio_2 ) { ?>
+	<img src="<?php echo $portfolio_2['url']; ?>" alt="<?php echo $portfolio_2['alt']; ?>" />
+<?php } ?>
+  </div>
+</div>
 <?php get_footer(); ?>
