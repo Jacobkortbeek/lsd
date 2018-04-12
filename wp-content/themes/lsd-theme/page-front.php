1531876f53e9a5b97c03a8d379cbbe70ff1b1e05
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 01:04:46 +0100

    front page start php

--- wp-content/themes/lsd-theme/page-front.php
+++ wp-content/themes/lsd-theme/page-front.php
@@ -25,22 +25,13 @@
 
 <div class="row">
   <div class="col-md-4">
-    <?php $image_one = get_field( 'image_one' ); ?>
-<?php if ( $image_one ) { ?>
-	<img src="<?php echo $image_one['url']; ?>" alt="<?php echo $image_one['alt']; ?>" />
-<?php } ?>
+  1
   </div>
   <div class="col-md-4">
-    <?php $image_two = get_field( 'image_two' ); ?>
-<?php if ( $image_two ) { ?>
-	<img src="<?php echo $image_two['url']; ?>" alt="<?php echo $image_two['alt']; ?>" />
-<?php } ?>
+    2
   </div>
   <div class="col-md-4">
-    <?php $image_three = get_field( 'image_three' ); ?>
-<?php if ( $image_three ) { ?>
-	<img src="<?php echo $image_three['url']; ?>" alt="<?php echo $image_three['alt']; ?>" />
-<?php } ?>
+    3
   </div>
 </div>
 
