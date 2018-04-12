70c45bc01b36c8db62ae8e25872eada11d6953da
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-12 21:24:10 +0100

    about

--- wp-content/themes/lsd-theme/scss/variables/page-about.php
+++ wp-content/themes/lsd-theme/scss/variables/page-about.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
-
+<div class="row">
+  <div class="span12">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+      <?php endwhile; else : ?>
+        <p><?php _e ( 'Sorry, no post matched your criteria.' ); ?>
+      <?php endif; ?>
+  </div>
+</div>
 
 <?php get_footer(); ?>
