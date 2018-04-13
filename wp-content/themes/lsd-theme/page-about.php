a56478f3c4a6b2b56345256e9447430b1916c1d5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-13 17:04:31 +0100

    about

--- wp-content/themes/lsd-theme/page-about.php
+++ wp-content/themes/lsd-theme/page-about.php
@@ -8,7 +8,7 @@
 
 <div class="row">
   <div class="span12">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
       <?php endwhile; else : ?>
         <p><?php _e ( 'Sorry, no post matched your criteria.' ); ?>
