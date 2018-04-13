8ab8c6bcfc20924c3009fdfe6dab1151b6be8534
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-13 17:08:36 +0100

    about

--- wp-content/themes/lsd-theme/page-about.php
+++ wp-content/themes/lsd-theme/page-about.php
@@ -6,9 +6,15 @@
 
 <?php get_header(); ?>
 
+<?php
+
+  $about_title = get_field( 'about_title' );
+
+?>
+
 <div class="row">
   <div class="span12">
-      <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
       <?php endwhile; else : ?>
         <p><?php _e ( 'Sorry, no post matched your criteria.' ); ?>
@@ -19,8 +25,8 @@
 <div class="row">
   <div class="span12">
     <p>Hello World</p>
-    <?php if ( the_field( 'about_title' ) ): ?>
-      <h2> <?php the_field( 'about_title' ); ?> </h2>
+    <?php if ( $about_title ) ): ?>
+      <h2> <?php $about_title ?> </h2>
     <?php else: ?>
       <p><?php _e( 'this is an error message!!' ); ?></p>
     <?php endif; ?>
